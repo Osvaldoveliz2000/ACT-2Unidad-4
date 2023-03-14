091fe38b37ff91d9b9e5df0aab59871e2310b5e2
Osvaldoveliz2000 <125836784+Osvaldoveliz2000@users.noreply.github.com>
2023-03-14 00:00:00 -0600

    Add files via upload

---
+++ Actividad 2 Uni-4.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+// Definición de la clase base
+public class Animal
+{
+    public string nombre;
+    public int edad;
+
+    public Animal(string nombre, int edad)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void hacerSonido()
+    {
+        Console.WriteLine("El animal está haciendo un sonido.");
+    }
+}
+
+// Definición de la clase derivada
+public class Perro : Animal
+{
+    public string raza;
+
+    public Perro(string nombre, int edad, string raza) : base(nombre, edad)
+    {
+        this.raza = raza;
+    }
+
+    public void ladrar()
+    {
+        Console.WriteLine("El perro está ladrando.");
+    }
+}
+
+// Uso de la clase derivada
+public class Program
+{
+    public static void Main()
+    {
+        Perro miPerro = new Perro("Fido", 5, "Labrador");
+        Console.WriteLine("Nombre: " + miPerro.nombre);
+        Console.WriteLine("Edad: " + miPerro.edad);
+        Console.WriteLine("Raza: " + miPerro.raza);
+        miPerro.hacerSonido();
+        miPerro.ladrar();
+    }
+}
